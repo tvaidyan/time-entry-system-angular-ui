607efce827da0eba10c8623a62de58c7cbeae755
Tom Vaidyan <tvaidyan@gmail.com>
2018-08-16 23:04:35 -0400

    scaffold calendar view

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarEvent } from 'calendar-utils';
+import { subDays, addDays } from 'date-fns';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent implements OnInit {
+export class CalendarComponent {
+  colors: any = {
+    red: {
+      primary: '#ad2121',
+      secondary: '#FAE3E3'
+    },
+    blue: {
+      primary: '#1e90ff',
+      secondary: '#D1E8FF'
+    },
+    yellow: {
+      primary: '#e3bc08',
+      secondary: '#FDF1BA'
+    }
+  };
 
-  constructor() { }
+  
+  view: string = 'month';
 
-  ngOnInit() {
+  viewDate: Date = new Date('2016-01-05');
+
+  events: CalendarEvent[] = [
+    {
+      start: new Date('2016-01-08'),
+      end: new Date('2016-01-10'),
+      title: 'One day excluded event',
+      color: this.colors.red
+    },
+    {
+      start: new Date('2016-01-01'),
+      end: new Date('2016-01-09'),
+      title: 'Multiple weeks event'
+    }
+  ];
+
+  // exclude weekends
+  excludeDays: number[] = [0, 6];
+
+  skipWeekends(direction: 'back' | 'forward'): void {
+    if (this.view === 'day') {
+      if (direction === 'back') {
+        while (this.excludeDays.indexOf(this.viewDate.getDay()) > -1) {
+          this.viewDate = subDays(this.viewDate, 1);
+        }
+      } else if (direction === 'forward') {
+        while (this.excludeDays.indexOf(this.viewDate.getDay()) > -1) {
+          this.viewDate = addDays(this.viewDate, 1);
+        }
+      }
+    }
   }
-
 }
