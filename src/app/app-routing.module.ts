83cb97920be1e17e61d4c357ad6ecd4ddf2cda2a
Tom Vaidyan <tvaidyan@gmail.com>
2018-08-17 22:29:50 -0400

    remove unused component import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { TimeSheetComponent } from './time-sheet/time-sheet.component';
 import { AuthenticatedUserComponent } from './authenticated-user/authenticated-user.component';
 import { CalendarComponent } from './calendar/calendar.component';
