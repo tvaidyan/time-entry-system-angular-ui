78fff721530b856e4084cffe88ad1cf6ff0224f7
Tom Vaidyan <tvaidyan@gmail.com>
2018-09-14 21:36:38 -0400

    add routing for projects

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { TimeSheetComponent } from './time-sheet/time-sheet.component';
-import { AuthenticatedUserComponent } from './authenticated-user/authenticated-user.component';
-import { CalendarComponent } from './calendar/calendar.component';
-import { EmployeesComponent } from './employees/employees.component';
-import { AddEmployeeComponent } from './add-employee/add-employee.component';
-import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
-import { ClientsComponent } from './clients/clients.component';
-import { AddClientComponent } from './add-client/add-client.component';
-import { EditClientComponent } from './edit-client/edit-client.component';
-import { AuthGuard } from './services/auth-guard.service';
-import { DepartmentsComponent } from './departments/departments.component';
-import { AddDepartmentComponent } from './add-department/add-department.component';
-import { EditDepartmentComponent } from './edit-department/edit-department.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { TimeSheetComponent } from "./time-sheet/time-sheet.component";
+import { AuthenticatedUserComponent } from "./authenticated-user/authenticated-user.component";
+import { CalendarComponent } from "./calendar/calendar.component";
+import { EmployeesComponent } from "./employees/employees.component";
+import { AddEmployeeComponent } from "./add-employee/add-employee.component";
+import { EditEmployeeComponent } from "./edit-employee/edit-employee.component";
+import { ClientsComponent } from "./clients/clients.component";
+import { AddClientComponent } from "./add-client/add-client.component";
+import { EditClientComponent } from "./edit-client/edit-client.component";
+import { AuthGuard } from "./services/auth-guard.service";
+import { DepartmentsComponent } from "./departments/departments.component";
+import { AddDepartmentComponent } from "./add-department/add-department.component";
+import { EditDepartmentComponent } from "./edit-department/edit-department.component";
+import { AddProjectComponent } from "./add-project/add-project.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { EditProjectComponent } from "./edit-project/edit-project.component";
 
 const routes: Routes = [
-
   { path: "login", component: LoginComponent },
   {
     path: "authenticated",
@@ -40,7 +42,10 @@ const routes: Routes = [
       {
         path: "edit-client/:id",
         component: EditClientComponent
-      }
+      },
+      { path: "projects", component: ProjectsComponent },
+      { path: "add-project", component: AddProjectComponent },
+      { path: "edit-project/:id", component: EditProjectComponent }
     ]
   },
   { path: "", component: LoginComponent },
@@ -52,4 +57,4 @@ const routes: Routes = [
   exports: [RouterModule],
   declarations: [TimeSheetComponent]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
